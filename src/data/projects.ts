c95cb4db5eab7ecff3d69dfa07e65f5ff21f106a
Sebastian Sarmiento Bazzani <sebasstudio@Sebastians-Mac-Studio.local>
2026-08-21 20:26:37 -0500

    First portfolio version

---
+++ src/data/projects.ts
@@ -0,0 +1,100 @@
+export const projects = [
+  // HERO
+  {
+    slug: "opentowork",
+    title: "Open to Work",
+    cover: "/images/projects/parkour.jpg",
+  },
+
+  {
+    slug: "startup-1947",
+    title: "La Startup de 1947",
+    cover: "/images/projects/startup-1947.png",
+  },
+
+  {
+    slug: "playingtowin",
+    title: "PLAYING TO WIN",
+    cover: "/images/projects/playingtowin.jpg",
+  },
+
+  {
+    slug: "ndc",
+    title: "Negocios desde Cero",
+    cover: "/images/projects/ndc.jpg",
+  },
+
+  // FEATURE
+
+  {
+    slug: "lacasadelaplata",
+    title: "La Casa de la Plata",
+    cover: "/images/projects/lacasadelaplata.jpg",
+  },
+
+  {
+    slug: "ttecrebrand",
+    title: "TTEC Rebrand",
+    cover: "/images/projects/ttecrebrand.jpg",
+  },
+
+  {
+    slug: "lahistoria",
+    title: "La Historia Detrás de las Cosas",
+    cover: "/images/projects/lahistoria.jpg",
+  },
+
+  // SMALL
+
+  {
+    slug: "stellantis",
+    title: "Stellantis",
+    cover: "/images/projects/stellantis.png",
+  },
+
+  {
+    slug: "clumsychef1",
+    title: "Learn English with Platzi",
+    cover: "/images/projects/clumsychef1.jpg",
+  },
+
+  {
+    slug: "objet",
+    title: "Objet Spec Ad",
+    cover: "/images/projects/objet.png",
+  },
+
+  // FILMS
+
+  {
+    slug: "mysteryland",
+    title: "Mysteryland",
+    cover: "/images/projects/mysteryland.png",
+  },
+
+  {
+    slug: "peephole",
+    title: "Peephole",
+    cover: "/images/projects/peephole.png",
+  },
+
+  {
+    slug: "lfdo",
+    title: "La Fábrica de Octopus",
+    cover: "/images/projects/lfdo.jpg",
+  },
+
+  // POSTERS
+
+  {
+    slug: "sisters",
+    title: "Hermanas",
+    cover: "/images/projects/sisters.jpg",
+  },
+
+  {
+    slug: "anfny",
+    title: "Antofagasta, New York",
+    cover: "/images/projects/anfny.jpg",
+  },
+];
